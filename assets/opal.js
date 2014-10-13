3a1f421438a8a50c769ea5497e4128a794f0db7b
David Miller <david@deadpansincerity.com>
2014-10-13 16:38:09 +0000

    update

---
+++ assets/opal.js
@@ -0,0 +1,24 @@
+function get_started(what){
+    event.preventDefault();
+    email = $('#'+what).val()
+    $.post('http://ohcucontakt.deadpansincerity.com/former.php', {
+        z_name: 'Unknown',
+        z_requester: email,
+        z_subject: 'OPAL website quick form',
+        z_description: 'More information about OPAL'
+    });
+    return false;
+}
+
+
+function contactform(){
+    event.preventDefault();
+    
+    $.post('http://ohcucontakt.deadpansincerity.com/former.php', {
+        z_name: $('#name').val(),
+        z_requester: $('#email').val(),
+        z_subject: 'OPAL website contact form',
+        z_description: $('#message').val(),
+    });
+    return false;
+}
